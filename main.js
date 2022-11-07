654c4c456bf66d9987937002c512a844bf0cac83
willaimRodrigo <billtechnew@gmail.com>
2022-11-07 09:45:20 -0300

    index recovery

--- main.js
+++ main.js
@@ -8,32 +8,30 @@ function createGame(player1, hour, player2) {
   `
 }
 
-let delay = -0.4;
+let delay = -0.4
 
 function createCard(date, day, games) {
-  delay = delay + 0.4;
+  delay = delay + 0.4
   return `
   <div class="card" style="animation-delay: ${delay}s">
         <h2>${date} <span>${day}</span></h2>
           <ul>
             ${games}
           </ul>
-        </div>
-  `
+        </div> `
 }
 
-document.querySelector("#cards").innerHTML = 
-      createCard("24/11", "quinta", createGame("brazil", "16:00", "serbia")) +
-      createCard(
-        "28/11",
-        "segunda",
-        createGame("brazil", "13:00", "switzerland") +
-          createGame("portugal", "16:00", "uruguai")
-      ) +
-      createCard(
-        "02/12",
-        "sexta",
-        createGame("brazil", "16:00", "cameroon") +
-          createGame("serbia", "16:00", "switzerland")
-      )
-
+document.querySelector("#cards").innerHTML =
+  createCard("24/11", "quinta", createGame("brazil", "16:00", "serbia")) +
+  createCard(
+    "28/11",
+    "segunda",
+    createGame("brazil", "13:00", "switzerland") +
+      createGame("portugal", "16:00", "uruguai")
+  ) +
+  createCard(
+    "02/12",
+    "sexta",
+    createGame("brazil", "16:00", "cameroon") +
+      createGame("serbia", "16:00", "switzerland")
+  )
